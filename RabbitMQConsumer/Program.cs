faaadc4b0427a02341cc2919968cd08c2bc47991
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2021-01-27 16:44:57 +0600

    RabbitMQ Demo Codes

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread.Sleep(1000);
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            string queueName = "demo";
+            var rabbitMqConnection = factory.CreateConnection();
+            var rabbitMqChannel = rabbitMqConnection.CreateModel();
+
+            rabbitMqChannel.QueueDeclare(queue: queueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            rabbitMqChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            int messageCount = Convert.ToInt16(rabbitMqChannel.MessageCount(queueName));
+            Console.WriteLine(" Listening to the queue. This channels has {0} messages on the queue", messageCount);
+
+            var consumer = new EventingBasicConsumer(rabbitMqChannel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Message Received: " + message);
+                rabbitMqChannel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                Thread.Sleep(1000);
+            };
+            rabbitMqChannel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Thread.Sleep(1000 * messageCount);
+            Console.WriteLine(" Connection closed, no more messages.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "demo", false, false, false, null);
+                    channel.QueueDeclare(queue: "demo2", false, false, false, null);
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        string message = $"Hello RabbitMQ {i + 1}";
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: string.Empty, routingKey: "demo", basicProperties: null, body: body);
+                        channel.BasicPublish(exchange: string.Empty, routingKey: "demo2", basicProperties: null, body: body);
+                    }
+                }
+            }
+        }
+    }
+}
